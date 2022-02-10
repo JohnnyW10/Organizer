569d9981accd9d4edb6df9ac6e313632eb5997c4
JohnnyW10 <60486661+JohnnyW10@users.noreply.github.com>
2022-02-10 22:57:57 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,51 +4,9 @@ import './styles/main.css';
 import nav_buttons from './components/header.js';
 import Update from './components/activityList.js';
 import ShopingList from './components/shoppingList.js';
+import LoggedButton from './components/loginButton';
 import React from 'react';
  
-
-const inputs = [
-  {
-    name: 'Name: ',
-    ID: 'input_name',
-    c_Name: 'c_inputs',
-    type: 'text',
-    placeholder: 'Name'
-  },
-  {
-    name: 'From: ',
-    ID: 'input_from_time',
-    c_Name: 'c_inputs',
-    type: 'time',
-    placeholder: '07:00'
-  },
-  {
-    name: 'To: ',
-    ID: 'input_to_time',
-    c_Name: 'c_inputs',
-    type: 'time',
-    placeholder: '08:00'
-  },
-  {
-    name: 'Importance Level: ',
-    ID: 'input_Ilevel',
-    c_Name: 'c_inputs',
-    type: 'text',
-    placeholder: 'Chose the level, (high, low, medium)'
-  },
-  {
-    name: 'Desciption: ',
-    ID: 'input_descript',
-    c_Name: 'c_inputs',
-    type: 'textarea',
-    placeholder: 'Describe your activities...'
-  }
-].map((object_m) => {
-  return (
-    <p key={object_m.name}>{object_m.name}<input id={object_m.ID} className={object_m.c_Name} type={object_m.type} placeholder={object_m.placeholder}/></p>
-  );
-});
-
 
 
 function App() {
@@ -57,12 +15,9 @@ function App() {
       <header className="App-header">
         <div className="title"><a href="index.html"><p>Organizer</p></a></div>
         {nav_buttons}
+        <LoggedButton />
       </header>
       <main>
-        <div className="add_activity">
-          <h3>Add your activity!</h3>
-          {inputs}
-        </div>
         <div className='list_of_activities'>
           <h2>List of activites</h2>
           <Update />
